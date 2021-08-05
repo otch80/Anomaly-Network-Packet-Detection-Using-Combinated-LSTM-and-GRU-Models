dacd01c66befb5f4cf092021118c6d57a6d2d2fa
otch80 <minkk1224@gmail.com>
2021-08-05 13:30:18 +0900

    main

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import PacketPreprocess
-import DetectModels
+from PacketPreprocess import Preprocess
+import packetpreprocess
+import detectmodels
 
 if __name__ == "__main__":
-    ppc = PacketPreprocess()
-    ppc.loadCsv()
-    ppc.missingValue()
-    lstmZip_df = ppc.normalizeToLstm()
+    preprocess = packetpreprocess.Preprocess()
+    detectmodel = detectmodels.DetectModels()
+
+    preprocess.loadCsv("dataset/train_df.csv", "dataset/label_df.csv")
+    preprocess.missingValue()
+    train_df, label_df = preprocess.normalizeToModel(timesteps=10)
+    train_df, label_df = preprocess.reshapeToMultyShape(train_df, label_df)
+
+    train_ae_df, test_ae_df = preprocess.normalizeToAE(timesteps=10)
+    train_ae_df, test_ae_df = preprocess.reshapeToMultyShape(train_ae_df, test_ae_df)
+
+    train_df, test_df = detectmodel.trainTestSplit(train_df)
+    train_ae_df, test_ae_df = detectmodel.trainTestSplit(train_ae_df)
+
+    lstm, lstm_history = detectmodel.LSTM()
+    lstm_cnn, lstm_cnn_history = detectmodel.LSTM_CNN()
+    lstm_ae, lstm_ae_history = detectmodel.LSTM_AE()
+    gru, gru_history = detectmodel.GRU()
+    gru_cnn, gru_cnn_history = detectmodel.GRU_CNN()
+    gru_ae, gru_ae_history = detectmodel.GRU_AE()
